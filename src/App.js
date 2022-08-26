87a8725939640ee3c898b173880be97d0d01b949
Fouad El Metioui <fouadpro2002@gmail.com>
2022-08-26 18:50:08 +0100

    finish project --success!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
+import React, { Component } from 'react'
+
 import logo from './logo.svg';
 import './App.css';
 import TodoList from './components/TodoList'
 import TodoInput from './components/TodoInput'
 import Todoitems from './components/Todoitems'
+import { v1 as uuid } from "uuid";
 
 
 
+class App extends Component {
+  state = {
+    items: [],
+    id: uuid(),
+    item: '',
+    editItem: false,
+  }
 
-function App() {
-  return (
-    <div className="App">
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    }
+    const updateItems = [...this.state.items, newItem]
+    this.setState({
+      items: updateItems,
+      item: '',
+      id: uuid(),
+      editItem: false
+
+    },
+      () => console.log(this.state)
+    );
+  }
+
+  clearList = () => {
+    this.setState({
+      items: [],
+      id: uuid(),
+      item: '',
+      editItem: false,
+    })
+  }
+
+  handleDelete = id => {
+    const filterItems = this.state.items.filter(item =>
+      item.id !== id);
+      this.setState({
+        items:filterItems
+      })
+
+  }
+
+  handleEdit = id => {
+    const filterItems = this.state.items.filter(item =>
+      item.id !== id);
+      const selectedItem = this.state.items.find(item => item.id === id)
+      this.setState({
+        items: filterItems,
+        item:selectedItem.title,
+        id:id,
+        editItem:true
+      })
+
+  }
+
+
+  render() {
+    console.log(this.state)
+    return (
       <div className="container">
         <div className="row">
-              <TodoInput/>
-              <TodoList/>
-              {/* <Todoitems/> */}
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleEdit={this.handleEdit}
+              handleDelete={this.handleDelete}
+            />
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
 
+    )
+  }
+}
 export default App;
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,8 +2,29 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return (
-      <div>hello from TodoInput</div>
+      <div className="card card-body my-3">
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text bg-primary text-white">
+                <i className="fa fa-book" />
+              </div>
+            </div>
+            <input type="text" className="form-control text-capitalize" placeholder="add to do item" value={item}
+              onChange={handleChange}
+            />
+
+          </div>
+          <button type="submit" className={editItem?"btn btn-block btn-warning mt-3 text-uppercase mx-5":"btn btn-block btn-primary mt-3 text-uppercase mx-5"}
+          disabled={item?false:true}
+          style={{ width: '90%' }}>
+            {editItem ? "edit item" : "add item"}
+          </button>
+        </form>
+
+      </div>
     )
   }
 }
